79b656a0178ef7cbb773fae48d7d1d080e62d602
Rafael <41241789@mackenzista.com.br>
2021-10-18 19:56:35 -0300

    start repository

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'app.dart';
+import 'counter/counter_observer.dart';
+import 'package:bloc/bloc.dart';
+
+void main() {
+  // BlocObserver observer = CounterObserver();
+  runApp(const CounterApp());
+}
